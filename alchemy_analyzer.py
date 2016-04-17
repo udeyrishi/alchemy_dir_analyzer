016b56e38a94e3329e93b13bb7c884461879aafc
Udey Rishi <udey.rishi@gmail.com>
2016-04-17 14:21:43 -0600

    Removed dead comment

--- alchemy_analyzer.py
+++ alchemy_analyzer.py
@@ -1,11 +1,11 @@
 # Copyright 2016 Udey Rishi
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,7 +40,6 @@ class AlchemyFileAnalyzer(object):
         self.transaction_limit_callback = transaction_limit_callback
 
     def analyze(self, file_name):
-        # temp place holder
         result = dict()
         with open(file_name, 'r') as f:
             text = f.read()
